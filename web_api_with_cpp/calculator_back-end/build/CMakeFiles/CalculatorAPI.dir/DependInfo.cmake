3ae37444299af078e79d58df89a466a70d8d1fd8
nnnw1 <gla94@sfu.ca>
2023-06-12 14:44:59 -0700

    push my branch

---
+++ web_api_with_cpp/calculator_back-end/build/CMakeFiles/CalculatorAPI.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ligang/Desktop/program/projects/web_data_room/Web-data-room/web_api_with_cpp/calculator_back-end/CalcResourceFactory.cpp" "CMakeFiles/CalculatorAPI.dir/CalcResourceFactory.cpp.o" "gcc" "CMakeFiles/CalculatorAPI.dir/CalcResourceFactory.cpp.o.d"
+  "/Users/ligang/Desktop/program/projects/web_data_room/Web-data-room/web_api_with_cpp/calculator_back-end/CalcService.cpp" "CMakeFiles/CalculatorAPI.dir/CalcService.cpp.o" "gcc" "CMakeFiles/CalculatorAPI.dir/CalcService.cpp.o.d"
+  "/Users/ligang/Desktop/program/projects/web_data_room/Web-data-room/web_api_with_cpp/calculator_back-end/CalcServiceSettingsFactory.cpp" "CMakeFiles/CalculatorAPI.dir/CalcServiceSettingsFactory.cpp.o" "gcc" "CMakeFiles/CalculatorAPI.dir/CalcServiceSettingsFactory.cpp.o.d"
+  "/Users/ligang/Desktop/program/projects/web_data_room/Web-data-room/web_api_with_cpp/calculator_back-end/main.cpp" "CMakeFiles/CalculatorAPI.dir/main.cpp.o" "gcc" "CMakeFiles/CalculatorAPI.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
